9d070b69172609a7dc34de7ee1d04974e1b862ac
Gonzalo Perelstein <36910726+gonmp@users.noreply.github.com>
2020-09-25 12:20:17 -0300

    [FIVE-9] GP:
    -Agregados paquetes AutoMapper, AutoMapper.Extensions.Microsoft.Extensions.DependencyInjection en la capa DTOS
    -Creada la clase AutoMapperProfile en la capa DTOS
    -Creada la clase abstracta BaseApiController

---
+++ FiveRockingFingers/FRF.Web.Dtos/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FRF.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FRF.Web.Dtos
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Core.Models.Project, ProjectDto>()
+                .ReverseMap();
+            CreateMap<Core.Models.Project, ProjectCategoryDTO>()
+                .ReverseMap();
+        }
+    }
+}
---
+++ FiveRockingFingers/FRF.Web/Controllers/BaseApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FRF.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public abstract class BaseApiController<T> : ControllerBase where T : class
+    {
+        public abstract IActionResult Get(int id);
+        public abstract IActionResult GetAll();
+        public abstract IActionResult Save(T entity);
+        public abstract IActionResult Update(int id, T entity);
+        public abstract IActionResult Delete(int id);
+    }
+}
