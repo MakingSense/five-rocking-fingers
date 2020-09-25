c1a0c7374a80739ee38f7f051a2740609efef3c7
Gonzalo Perelstein <36910726+gonmp@users.noreply.github.com>
2020-09-25 12:23:13 -0300

    [FIVE-9] GP:
    -Modificado el ProjectDto
    -Modificado el ProjectsController

--- FiveRockingFingers/FRF.Web.Dtos/Projects/ProjectDTO.cs
+++ FiveRockingFingers/FRF.Web.Dtos/Projects/ProjectDTO.cs
@@ -4,5 +4,24 @@ namespace FRF.Web.Dtos
 {
     public class ProjectDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public string Client { get; set; }
+        public int Budget { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int CategoryId { get; set; }
+        public ProjectCategoryDTO ProjectCategoryDTO { get; set; }
+        public bool isActive { get; set; }
+        public DateTime StartDate { get; set; }
+
+    }
+
+    public class ProjectCategoryDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
--- FiveRockingFingers/FRF.Web/Controllers/ProjectsController.cs
+++ FiveRockingFingers/FRF.Web/Controllers/ProjectsController.cs
@@ -1,22 +1,148 @@
-﻿using FRF.Core.Services;
+﻿using AutoMapper;
+using FRF.Core.Services;
+using FRF.Web.Controllers;
+using FRF.Web.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 
 namespace FiveRockingFingers.Controllers
 {
-    [Route("api/[controller]/[Action]")]
-    [ApiController]
-    public class ProjectsController : ControllerBase
+    public class ProjectsController : BaseApiController<ProjectDto>
     {
+        private readonly IMapper mapper;
+
         public IProjectsService ProjectService { get; set; }
-        public ProjectsController(IProjectsService projectsService)
+        public ProjectsController(IProjectsService projectsService, IMapper mapper)
         {
             this.ProjectService = projectsService;
+            this.mapper = mapper;
         }
 
+
         [HttpGet]
-        public IActionResult Get()
+        override public IActionResult GetAll()
         {
-            return Ok(ProjectService.GetAll());
-        }        
+            try
+            {
+                var projects = ProjectService.GetAll();
+
+                Console.WriteLine("LLegue hasta acá");
+
+                var projectsDto = mapper.Map<IEnumerable<ProjectDto>>(projects);
+
+                return Ok(projectsDto);
+            }
+            catch
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+        
+        [HttpGet("{id}")]
+        override public IActionResult Get(int id)
+        {
+            try
+            {
+                var project = ProjectService.Get(id);
+
+                if (project == null)
+                {
+                    return NotFound();
+                }
+
+                var projectDto = mapper.Map<ProjectDto>(project);
+
+                return Ok(projectDto);
+            }
+            catch
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        [HttpPost]
+        override public IActionResult Save(ProjectDto projectDto )
+        {
+            if(projectDto == null)
+            {
+                return BadRequest("Project object is null");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest("Invalid model object");
+            }
+
+            var project = mapper.Map<FRF.Core.Models.Project>(projectDto);
+
+            try
+            {
+                ProjectService.Save(project);
+
+                return CreatedAtRoute("DefaultApi", new { id = project.Id }, project);
+            }
+            catch
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+            
+        }
+        
+        [HttpPut("{id}")]
+        override public IActionResult Update(int id, ProjectDto projectDto)
+        {
+            if(projectDto == null)
+            {
+                return BadRequest("Project object is null");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid model object");
+            }
+
+            try
+            {
+                var project = ProjectService.Get(id);
+
+                if(project == null)
+                {
+                    return NotFound();
+                }
+
+                mapper.Map(projectDto, project);
+
+                ProjectService.Update(project);
+
+                return NoContent();
+            }
+            catch
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        override public IActionResult Delete(int id)
+        {
+            try
+            {
+                var project = ProjectService.Get(id);
+
+                if(project == null)
+                {
+                    return NotFound();
+                }
+
+                ProjectService.Delete(id);
+
+                return NoContent();
+            }
+            catch
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
     }
 }
