e6dd9605a258e1a082edca6cf004f8e30f03c39d
Gonzalo Perelstein <gperelstein@makingsense.com>
2020-10-14 09:28:56 -0300

    [FIVE-12] GP:
    -Se crearon los EntityModelos de Artifact y ArtifactType
    -Se crearon los Models de Artifact y ArtifactType

---
+++ FiveRockingFingers/FRF.Core/Models/Artifact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace FRF.Core.Models
+{
+    public class Artifact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Provider { get; set; }
+        public XElement Settings { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+        public int ArtifactTypeId { get; set; }
+        public ArtifactType ArtifactType { get; set; }
+    }
+}
---
+++ FiveRockingFingers/FRF.Core/Models/ArtifactType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FRF.Core.Models
+{
+    public class ArtifactType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Artifact> Artifacs { get; set; }
+    }
+}
