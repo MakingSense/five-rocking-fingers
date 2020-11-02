6a3e6087cf1f5d875d1461a5246d7b4711fdc406
Gonzalo Perelstein <gperelstein@makingsense.com>
2020-11-02 14:26:00 -0300

    [FIVE-165] GP: Se creó el dialogo de confirmación (components/ArtifactsComponents/ConfirmationDialog) al momento de borrar un artefacto de un proyecto

--- FiveRockingFingers/FRF.Web/ClientApp/src/components/ArtifactsComponents/ArtifactsTableRow.tsx
+++ FiveRockingFingers/FRF.Web/ClientApp/src/components/ArtifactsComponents/ArtifactsTableRow.tsx
@@ -1,22 +1,31 @@
 ﻿import * as React from 'react';
 import Artifact from '../../interfaces/Artifact';
 import { Button } from 'reactstrap';
+import ConfirmationDialog from './ConfirmationDialog';
 
 const ArtifactsTableRow = (props: { artifact: Artifact, deleteArtifact: Function }) => {
 
+    const [openConfirmDialog, setOpenConfirmDialog] = React.useState(false);
+
+    
+
     const deleteButtonClick = () => {
-        props.deleteArtifact(props.artifact.id)
+        setOpenConfirmDialog(true);
     }
 
     return (
-        <tr>
-            <td>{props.artifact.name}</td>
-            <td>{props.artifact.provider}</td>
-            <td>{props.artifact.artifactType.name}</td>
-            <td>
-                <Button color="danger" onClick={deleteButtonClick}>Borrar</Button>
-            </td>
-        </tr>
+        <React.Fragment>
+            <tr>
+                <td>{props.artifact.name}</td>
+                <td>{props.artifact.provider}</td>
+                <td>{props.artifact.artifactType.name}</td>
+                <td>
+                    <Button color="danger" onClick={deleteButtonClick}>Borrar</Button>
+                </td>
+            </tr>
+            <ConfirmationDialog open={openConfirmDialog} setOpen={setOpenConfirmDialog} artifactToDelete={props.artifact} deleteArtifact={props.deleteArtifact} />
+
+         </React.Fragment>
     );
 };
 
---
+++ FiveRockingFingers/FRF.Web/ClientApp/src/components/ArtifactsComponents/ConfirmationDialog.tsx
@@ -0,0 +1,59 @@
+﻿import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Snackbar } from '@material-ui/core';
+import { Alert } from 'reactstrap';
+import axios from 'axios';
+import * as React from 'react';
+import Artifact from '../../interfaces/Artifact';
+import Swal from 'sweetalert2'
+
+
+const ConfirmationDialog = (props: { open: boolean, setOpen: Function, deleteArtifact: Function, artifactToDelete: Artifact }) => {
+
+
+
+    const handleClose = () => {
+        props.setOpen(false);
+    };
+
+    const handleConfirm = () => {
+        try {
+            props.deleteArtifact(props.artifactToDelete.id);
+            Swal.fire("El artefacto ha sido borrado con éxito", "", "success");
+        }
+        catch {
+            Swal.fire("Hubo un error al borrar el artefacto", "", "error");
+        }
+        handleClose();
+    };
+
+    const handleCancel = () => {
+        handleClose();
+    };
+
+    return (
+        <div>
+            <Dialog
+                open={props.open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"¿Está seguro que desea eliminar el artefacto?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        El proyecto <i>"{props.artifactToDelete.name}</i>" será eliminado
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleConfirm} color="primary">
+                        Confirmar
+                    </Button>
+                    <Button onClick={handleCancel} color="primary" autoFocus>
+                        Cancelar
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+
+export default ConfirmationDialog;
