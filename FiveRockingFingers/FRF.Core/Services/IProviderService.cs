61182394acf82895816a05ff7b2dc8c0ec9baa42
gonzalorom <gromero@makingsense.com>
2020-12-09 11:43:44 -0300

    [FIVE-214 DAL]GR:
    - Se creó interfaz IProviderService y servicio AwsProviderService para obtener información y gestionar solicitudes al API de AWS Pricing.
    - Se creó controller AwsProviderController con primer endpoint GetAllNamesAsync.
    - Se agregó service transient en Startup para inyección de dependencia.
    - Se agregó url en appsettings.Development.

---
+++ FiveRockingFingers/FRF.Core/Services/IProviderService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRF.Core.Services
+{
+    public interface IProviderService
+    {
+        Task<IList> GetAllAsync();
+        Task<List<KeyValuePair<string, string>>> GetAllNamesAsync();
+        Task<T> GetAsync<T>(string serviceCode);
+        Task<T> GetAsync<T>(string serviceCode,string nextToken);
+    }
+}
