94d71425421a15f5bbf7e63a960faa109dfe0ca6
gonzalorom <gromero@makingsense.com>
2020-12-11 13:58:24 -0300

    [FIVE-214]GR:
    - Se creó UnitTest para AwsArtifactsProviderService.
    - Se creó UnitTest para AwsArtifactsProviderController.

---
+++ test/FRF.Core.Tests/Services/AwsArtifactsProviderServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using FRF.Core.Base;
+using FRF.Core.Services;
+using Microsoft.Extensions.Options;
+using Moq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FRF.Core.Tests.Services
+{
+    public class AwsArtifactsProviderServiceTest
+    {
+        private readonly IOptions<AwsApiStringBase> _awsApiString;
+        private readonly AwsArtifactsProviderService _classUnderTest;
+
+        public AwsArtifactsProviderServiceTest()
+        {
+            var awsApiStringBase = new AwsApiStringBase(){UrlApiString = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json" };
+            _awsApiString = Options.Create(awsApiStringBase);
+            _classUnderTest = new AwsArtifactsProviderService(_awsApiString);
+        }
+
+        [Fact]
+        public async Task GetNamesAsync_ReturnList()
+        {
+            // Arange
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            
+            // Act
+            var result = await _classUnderTest.GetNamesAsync();
+
+            // Assert
+            var okResult = Assert.IsType<List<KeyValuePair<string, string>>>(result);
+
+            Assert.NotEmpty(okResult);
+        }
+    }
+}
---
+++ test/FRF.Web.Tests/Controllers/AwsArtifactsProviderControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using FRF.Core.Services;
+using FRF.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FRF.Web.Tests.Controllers
+{
+    public class AwsArtifactsProviderControllerTest
+    {
+        private readonly AwsArtifactsProviderController _classUnderTest;
+        private readonly Mock<IArtifactsProviderService> _artifactProviderService;
+
+        public AwsArtifactsProviderControllerTest()
+        {
+            _artifactProviderService = new Mock<IArtifactsProviderService>();
+            _classUnderTest= new AwsArtifactsProviderController(_artifactProviderService.Object);
+        }
+
+        private List<KeyValuePair<string, string>> CreateArtifactsNamesList()
+        {
+            var artifact1 = new KeyValuePair<string, string>("awsArtifact", "Aws Artifact");
+            var artifact2 = new KeyValuePair<string, string>("awsArtifact2", "Aws Artifact2");
+            var namesList = new List<KeyValuePair<string, string>>
+            {
+                artifact1,
+                artifact2
+            };
+
+            return namesList;
+        }
+
+        [Fact]
+        public async Task GetNamesAsync_ReturnsOkAndListOfArtifactsNames()
+        {
+            // Arrange
+            var artifactList = CreateArtifactsNamesList();
+            _artifactProviderService
+                .Setup(mock => mock.GetNamesAsync())
+                .ReturnsAsync(artifactList);
+
+            // Act
+            var result = await _classUnderTest.GetNamesAsync();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<List<KeyValuePair<string,string>>>(okResult.Value);
+
+            Assert.Contains(artifactList[0].Value, returnValue[0].Value);
+            Assert.Contains(artifactList[0].Key, returnValue[0].Key);
+
+            Assert.Contains(artifactList[1].Key, returnValue[1].Key);
+            Assert.Contains(artifactList[1].Key, returnValue[1].Key);
+        }
+    }
+}
