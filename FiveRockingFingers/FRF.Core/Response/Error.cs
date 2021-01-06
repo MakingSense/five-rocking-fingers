9ee0c9b508d03dba4bcb70705c21d0ff4d9824f8
martinmena <mmena@makingsense.com>
2021-01-06 11:44:24 -0300

    [FIVE-266] MM: Creadas clases ServiceResponse y Error en FRF.Core

---
+++ FiveRockingFingers/FRF.Core/Response/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace FRF.Core.Response
+{
+    public class Error
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ FiveRockingFingers/FRF.Core/Response/ServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace FRF.Core.Response
+{
+    public class ServiceResponse<T>
+    {
+        public bool Success { get; set; }
+        public Error Error { get; set; }
+        public T Value { get; set; }
+    }
+}
