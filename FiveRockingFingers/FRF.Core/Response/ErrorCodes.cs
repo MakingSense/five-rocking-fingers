bc3c7eb5a4b4360d472893647860740b68962dea
martinmena <mmena@makingsense.com>
2021-01-08 11:38:56 -0300

    [FIVE-266] MM: Creada clase ErrorCodes con la codificación de los errores de servicio.

---
+++ FiveRockingFingers/FRF.Core/Response/ErrorCodes.cs
@@ -0,0 +1,15 @@
+﻿namespace FRF.Core.Response
+{
+    static class ErrorCodes
+    {
+        // Not exists errors
+        public const int ArtifactNotExists = 1;
+        public const int ArtifactTypeNotExists = 2;
+        public const int CategoryNotExists = 3;
+        public const int ProjectNotExists = 4;
+
+        // Relation errors
+        public const int RelationAlreadyExisted = 10;
+        public const int RelationNotValid = 11;
+    }
+}
