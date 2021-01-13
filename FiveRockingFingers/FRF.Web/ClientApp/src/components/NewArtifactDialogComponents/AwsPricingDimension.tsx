55abda22cf659aa7ba4652144086c9795eee2caa
gperelstein <71278846+gperelstein@users.noreply.github.com>
2021-01-13 12:31:48 -0300

    [Five 235] (Frontend) Listar caracteristicas del artefacto seleccionado en la pagina de creacion de artefactos AWS (#62)
    Cambios realizados:
    -Se modificó el componente AwsForm para incorporar el tipo de artefacto
    -Se creó el componente SettingsAwsForm para que el usuario pueda seleccionar los atributos de su nuevo artefacto AWS
    -Se creó el componente AwsPricingDimension para que el usuario pueda seleccionar los términos relacionadas al pago de su componente AWS
    -Se creó el componente Confirmation para que el usuario pueda verificar todos los datos relacionados con su artefacto antes de guardarlo (este componente se confirmación su usó para artefactos AWS y artefactos Custom)

---
+++ FiveRockingFingers/FRF.Web/ClientApp/src/components/NewArtifactDialogComponents/AwsPricingDimension.tsx
@@ -0,0 +1,164 @@
+﻿import { Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, FormControlLabel, FormHelperText, InputLabel, MenuItem, Radio, RadioGroup, Select } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import * as React from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import ProviderArtifactSetting from '../../interfaces/ProviderArtifactSetting';
+import Setting from '../../interfaces/Setting';
+import KeyValueStringPair from '../../interfaces/KeyValueStringPair';
+import PricingTerm from '../../interfaces/PricingTerm';
+import ArtifactService from '../../services/ArtifactService';
+import AwsArtifactService from '../../services/AwsArtifactService';
+
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            display: 'flex',
+            flexWrap: 'wrap',
+        },
+        formControl: {
+            margin: theme.spacing(1),
+            width: '100%'
+        },
+        inputF: {
+            padding: 2,
+            marginTop: 10
+        },
+        circularProgress: {
+            width: '30%',
+            margin: 'auto'
+        }
+    }),
+);
+
+const AwsPricingDimension = (props: { showNewArtifactDialog: boolean, closeNewArtifactDialog: Function, name: string | null, handleNextStep: Function, handlePreviousStep: Function, settingsList: Setting[], setSettingsList: Function, settingsMap: { [key: string]: number[] }, setSettingsMap: Function, awsSettingsList: KeyValueStringPair[], settings: object, setSettings: Function, setAwsPricingTerm: Function, setSnackbarSettings: Function, setOpenSnackbar: Function }) => {
+
+    const classes = useStyles();
+    const { handleSubmit, control } = useForm();
+    const { showNewArtifactDialog, closeNewArtifactDialog, name, awsSettingsList } = props;
+
+    const [loading, setLoading] = React.useState<Boolean>(true);
+    const [awsPricingDimensionList, setPricingDimensionList] = React.useState<PricingTerm[]>([]);
+
+    React.useEffect(() => {
+        getPricingDimensions();
+    }, [name, awsSettingsList]);
+
+    const getPricingDimensions = async () => {
+        if (name) {
+            let statusOk = 200;
+            try {
+                setLoading(true);
+                const response = await AwsArtifactService.GetProductsAsync(name, awsSettingsList);
+                if (response.status === statusOk) {
+                    setPricingDimensionList(response.data);
+                    setLoading(false);
+                }
+            }
+            catch (error) {
+                props.setSnackbarSettings({ message: "Fuera de servicio. Por favor intente mas tarde.", severity: "error" });
+                props.setOpenSnackbar(true);
+                closeNewArtifactDialog();
+            }
+        }
+    }
+
+    //Create the artifact after submit
+    const handleConfirm = async (data: { term: number }) => {
+        if (data.term === null || data.term === undefined) return;
+        props.setAwsPricingTerm(awsPricingDimensionList[data.term]);
+        props.setSettings({ settings: createSettingsObject(data.term) });
+        props.handleNextStep();
+    }
+
+    const createSettingsObject = (index: number) => {
+        let settingFinalObject: object = {};
+        let settingsObject: { [key: string]: string } = {};
+
+        for (let i = 0; i < props.settingsList.length; i++) {
+            settingsObject[props.settingsList[i].name] = props.settingsList[i].value;
+        }
+        Object.assign(settingFinalObject, settingsObject, awsPricingDimensionList[index]);
+
+        return settingFinalObject;
+    }
+
+    const createPropertieLabel = (propertie: string, value: string | null) => {
+        if (value === null) return null;
+        return (
+            <Typography gutterBottom>
+                {propertie}: {value}
+            </Typography>
+        );
+    }
+
+    const pricingTermToString = (pricingTerm: PricingTerm) => {
+        return (
+            <React.Fragment>
+                {createPropertieLabel("Unit", pricingTerm.pricingDimension.unit)}
+                {createPropertieLabel("Lease Contract Length", pricingTerm.leaseContractLength)}
+                {createPropertieLabel("Purchase Option", pricingTerm.purchaseOption)}
+                {createPropertieLabel("Description", pricingTerm.pricingDimension.description)}
+                {createPropertieLabel("Currency", pricingTerm.pricingDimension.currency)}
+                {createPropertieLabel("Price per unit", pricingTerm.pricingDimension.pricePerUnit.toString())}
+                {createPropertieLabel("Term", pricingTerm.term)}
+                <hr/>
+            </React.Fragment>
+        );
+    }
+
+    const handleCancel = () => {
+        closeNewArtifactDialog();
+    }
+
+    const goPrevStep = () => {
+        props.handlePreviousStep();
+    }
+
+    return (
+        <Dialog open={showNewArtifactDialog}>
+            <DialogTitle id="alert-dialog-title">Formulario de artefactos AWS</DialogTitle>
+            <DialogContent>
+                <Typography gutterBottom>
+                    A continuación ingrese alguna de las opciones para adquirir su artefacto AWS
+                </Typography>
+                <form className={classes.container}>
+                    {loading ?
+                        <div className={classes.circularProgress}>
+                            <CircularProgress color="inherit" size={30} />
+                        </div> :
+                        awsPricingDimensionList.length === 0 ?
+                            <Typography gutterBottom>
+                                Lo sentimos, no hay productos según las especificaciones seleccionadas
+                            </Typography> :
+                            <Controller
+                                as={
+
+                                    <RadioGroup aria-label="term" name="term">
+                                        {awsPricingDimensionList.map((awsPricingTerm: PricingTerm, index: number) => {
+                                            return (
+                                                <FormControlLabel key={index} value={String(index)} control={<Radio />} label={pricingTermToString(awsPricingTerm)} />
+                                            );
+                                        })}
+                                    </RadioGroup>
+                                }
+                                name="term"
+                                rules={{ required: true }}
+                                control={control}
+                                defaultValue={''}
+                            />                   
+                    }
+                </form>
+
+            </DialogContent>
+            <DialogActions>
+                <Button size="small" color="primary" onClick={event => goPrevStep()}>Atrás</Button>
+                <Button size="small" color="primary" type="submit" onClick={handleSubmit(handleConfirm)}>Siguiente</Button>
+                <Button size="small" color="secondary" onClick={handleCancel}>Cancelar</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default AwsPricingDimension;
---
+++ FiveRockingFingers/FRF.Web/ClientApp/src/components/NewArtifactDialogComponents/Confirmation.tsx
@@ -0,0 +1,123 @@
+﻿import { Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import * as React from 'react';
+import { useForm } from 'react-hook-form';
+import Setting from '../../interfaces/Setting';
+import PricingTerm from '../../interfaces/PricingTerm';
+import ArtifactService from '../../services/ArtifactService';
+
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        title: {
+            fontWeight: 'bold'
+        },
+        settingName: {
+            fontStyle: 'italic'
+        }
+    }),
+);
+
+const Confirmation = (props: { showNewArtifactDialog: boolean, closeNewArtifactDialog: Function, provider: string | null, name: string | null, projectId: number, artifactTypeId: number | null, setOpenSnackbar: Function, setSnackbarSettings: Function, handlePreviousStep: Function, settingsList: Setting[], settings: object, updateList: Function, awsPricingTerm: PricingTerm | null }) => {
+
+    const classes = useStyles();
+    const { handleSubmit } = useForm();
+    const { showNewArtifactDialog, closeNewArtifactDialog, provider, name, projectId, artifactTypeId, setOpenSnackbar, setSnackbarSettings, settingsList, updateList } = props;
+
+    //Create the artifact after submit
+    const handleConfirm = async () => {
+        const artifactToCreate = {
+            name: name,
+            provider: provider,
+            artifactTypeId: artifactTypeId,
+            projectId: projectId,
+            settings: props.settings
+        };
+
+        try {
+            const response = await ArtifactService.save(artifactToCreate);
+            if (response.status === 200) {
+                setSnackbarSettings({ message: "El artefacto ha sido creado con éxito", severity: "success" });
+                setOpenSnackbar(true);
+                updateList();
+            } else {
+                setSnackbarSettings({ message: "Hubo un error al crear el artefacto", severity: "error" });
+                setOpenSnackbar(true);
+            }
+        }
+        catch (error) {
+            setSnackbarSettings({ message: "Hubo un error al crear el artefacto", severity: "error" });
+            setOpenSnackbar(true);
+        }
+        closeNewArtifactDialog();
+    }
+
+    const createPropertieLabel = (propertie: string, value: string | null) => {
+        if (value !== null) {
+            return (
+                <Typography gutterBottom>
+                    <span className={classes.settingName}>{propertie}</span>: {value}
+                </Typography>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    const handleCancel = () => {
+        closeNewArtifactDialog();
+    }
+
+    const goPrevStep = () => {
+        props.handlePreviousStep();
+    }
+
+    return (
+        <Dialog open={showNewArtifactDialog}>
+            <DialogTitle id="alert-dialog-title">Confirmación</DialogTitle>
+            <DialogContent>
+                <Typography gutterBottom>
+                    Revise las características de su nuevo artefacto y se está de acuerdo haga click en confirmar
+                </Typography>
+                <Typography gutterBottom>
+                    <span className={classes.title}>Nombre:</span> {name}
+                </Typography>
+                <Typography gutterBottom>
+                    <span className={classes.title}>Provedor:</span> {provider}
+                </Typography>
+                <Typography gutterBottom>
+                    <span className={classes.title}>Propiedades:</span>                    
+                </Typography>
+                {
+                    settingsList.map((setting: Setting, index: number) => {
+                        return (
+                            <Typography gutterBottom>
+                                <span className={classes.settingName}>{setting.name}</span>: {setting.value}
+                            </Typography>
+                        );
+                    })
+                }
+                {provider !== 'Custom' && props.awsPricingTerm !== null ?
+                    <React.Fragment>
+                        {createPropertieLabel("Unit", props.awsPricingTerm.pricingDimension.unit)}
+                        {createPropertieLabel("Lease Contract Length", props.awsPricingTerm.leaseContractLength)}
+                        {createPropertieLabel("Purchase Option", props.awsPricingTerm.purchaseOption)}
+                        {createPropertieLabel("Description", props.awsPricingTerm.pricingDimension.description)}
+                        {createPropertieLabel("Currency", props.awsPricingTerm.pricingDimension.currency)}
+                        {createPropertieLabel("Price per unit", props.awsPricingTerm.pricingDimension.pricePerUnit.toString())}
+                        {createPropertieLabel("Term", props.awsPricingTerm.term)}
+                    </React.Fragment>
+                    : null
+                }
+            </DialogContent>
+            <DialogActions>
+                <Button size="small" color="primary" onClick={event => goPrevStep()}>Atrás</Button>
+                <Button size="small" color="primary" type="submit" onClick={handleSubmit(handleConfirm)}>Finalizar</Button>
+                <Button size="small" color="secondary" onClick={handleCancel}>Cancelar</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default Confirmation;
