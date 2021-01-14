c4c4de2bb9e9ffdb8d90b01f376ce0a09f3acdac
Gonzalo Perelstein <gperelstein@makingsense.com>
2021-01-14 12:06:00 -0300

    [FIVE-121] GP:
    -Se creó la interfaz RelationCard
    -Se el componente ArtifactRelation

---
+++ FiveRockingFingers/FRF.Web/ClientApp/src/interfaces/ArtifactRelation.ts
@@ -0,0 +1,10 @@
+﻿import Artifact from './Artifact';
+import AwsArtifact from './AwsArtifact';
+
+export default interface ArtifactRelation {
+    artifact1: Artifact;
+    artifact2: Artifact;
+    setting1: AwsArtifact;
+    setting2: AwsArtifact;
+    relationTypeId: number;
+}
