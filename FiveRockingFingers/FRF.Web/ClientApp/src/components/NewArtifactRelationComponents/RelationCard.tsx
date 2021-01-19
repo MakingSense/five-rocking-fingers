b674394823b3e12da656546b8dd39531a8efd978
Gonzalo Perelstein <gperelstein@makingsense.com>
2021-01-19 13:40:55 -0300

    [FIVE-121-Frontend-1] GP-GR:
    -Se creó el componente ArtifactRelation para crear nuevas relaciones entre artefactos
    -Se creó el componente RelationCard para mostrar todas las relaciones que se estén por crear en el componente NewArtifactsRelation

---
+++ FiveRockingFingers/FRF.Web/ClientApp/src/components/NewArtifactRelationComponents/RelationCard.tsx
@@ -0,0 +1,49 @@
+﻿import * as React from 'react';
+import Typography from '@material-ui/core/Typography';
+import ArtifactRelation from '../../interfaces/ArtifactRelation';
+import SyncAltIcon from '@material-ui/icons/SyncAlt';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import { IconButton } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        inline: {
+            display: 'inline'
+        }
+    }),
+);
+
+const RelationCard = (props: { Relation: ArtifactRelation, index: number, deleteRelation: Function }) => {
+
+    const classes = useStyles();
+
+    const mapRelationTypeId = (id: number) => {
+        console.log(id);
+        switch (+id) {
+            case (0):
+                return <ArrowForwardIcon />;
+            case (1):
+                return <ArrowBackIcon />;
+            case (2):
+                return <SyncAltIcon />;
+            default:
+                return null;
+        }
+    }
+       
+    return (
+        <div>
+            <Typography className={classes.inline} gutterBottom>
+                {props.Relation.artifact1.name}: {props.Relation.artifact1Property} {mapRelationTypeId(props.Relation.relationTypeId)} {props.Relation.artifact2.name}: {props.Relation.artifact2Property}
+            </Typography>
+            <IconButton className={classes.inline} onClick={event => props.deleteRelation(props.index)} aria-label="delete" color="secondary">
+                <DeleteIcon />
+            </IconButton>
+        </div>
+    );
+}
+
+export default RelationCard;
