c1c1c84a283aebad545c49e61419c5d2a7287dff
gperelstein <71278846+gperelstein@users.noreply.github.com>
2021-01-25 13:33:10 -0300

    [Five 121-Frontend-2] Crear pagina de relaciones entre artefactos (#82)
    [Five 121-Frontend-2] Crear pagina de relaciones entre artefactos (#82)
    -Se agregó el componente EditArtifactRelation para editar relaciones existentes
    
    [FIVE-121 frontend 3] Crear pagina de relaciones entre artefactos (#85)
    -Se creó componente DeleteArtifactsRelation.
    -Se agregó nueva ruta en Routes.
    -Se agregaron nuevos botones dentro de ArtifactsTable y ArtifactsTableRow.
    -Se agregó llamada a axios dentro de ArtifactsTable para obtener relaciones de artefactos.
    
    Autores: Gonzalo Romero <gromero@makingsense.com>, Gonzalo Perelstein <gperelstein@makingsense.com>

--- FiveRockingFingers/FRF.Web/ClientApp/src/components/ArtifactsComponents/ArtifactsTableRow.tsx
+++ FiveRockingFingers/FRF.Web/ClientApp/src/components/ArtifactsComponents/ArtifactsTableRow.tsx
@@ -2,9 +2,9 @@
 import Artifact from '../../interfaces/Artifact';
 import { Button } from 'reactstrap';
 import ConfirmationDialog from './ConfirmationDialog';
+import { Link } from 'react-router-dom';
 
 const ArtifactsTableRow = (props: { artifact: Artifact, openSnackbar: Function, updateList: Function }) => {
-
     const [openConfirmDialog, setOpenConfirmDialog] = React.useState(false);
 
     const deleteButtonClick = () => {
@@ -12,23 +12,39 @@ const ArtifactsTableRow = (props: { artifact: Artifact, openSnackbar: Function, 
     }
 
     return (
-        <React.Fragment>
-            <tr>
-                <td>{props.artifact.name}</td>
-                <td>{props.artifact.provider}</td>
-                <td>{props.artifact.artifactType.name}</td>
-                <td>
-                    <Button color="danger" onClick={deleteButtonClick}>Borrar</Button>
-                </td>
-            </tr>
-            <ConfirmationDialog
-                open={openConfirmDialog}
-                setOpen={setOpenConfirmDialog}
-                artifactToDelete={props.artifact}
-                openSnackbar={props.openSnackbar}
-                updateList={props.updateList}
-            />
-         </React.Fragment>
+      <>
+        <tr>
+          <td>{props.artifact.name}</td>
+          <td>{props.artifact.provider}</td>
+          <td>{props.artifact.artifactType.name}</td>
+          <td >
+            <Button
+                className="mx-3" 
+                style={{ minHeight: "32px", width: "20%" }}
+              color="danger"
+              onClick={deleteButtonClick}
+            >
+              Borrar
+            </Button>
+
+            <Button
+                style={{ minHeight: "32px", width: "20%" }}
+              color="info"
+              tag={Link}
+              to={`/projects/${props.artifact.projectId}/artifacts/${props.artifact.id}`}
+            >
+              Relaciones
+            </Button>
+          </td>
+        </tr>
+        <ConfirmationDialog
+          open={openConfirmDialog}
+          setOpen={setOpenConfirmDialog}
+          artifactToDelete={props.artifact}
+          openSnackbar={props.openSnackbar}
+          updateList={props.updateList}
+        />
+      </>
     );
 };
 
