5e2db8e60e37dd00419b24eaa1dba85df0c08cee
Gonzalo Perelstein <gperelstein@makingsense.com>
2021-01-25 17:59:27 -0300

    [FIVE-295] GP-GR-MM:
    -Se creó la interfaz IArtifactPricing para el calcular el precio de un artifacto
    -Se implementó la interfaz IArtifactPricing en el modelo de Core, Artifact
    -Se creó la interfaz IBudgetService, el BudgetController, BudgetService y sus tests

---
+++ FiveRockingFingers/FRF.Core/Services/BudgetService.cs
@@ -0,0 +1,31 @@
+﻿using FRF.Core.Response;
+using System.Threading.Tasks;
+
+namespace FRF.Core.Services
+{
+    public class BudgetService : IBudgetService
+    {
+        private readonly IArtifactsService _artifactsService;
+
+        public BudgetService(IArtifactsService artifactsService)
+        {
+            _artifactsService = artifactsService;
+        }
+
+        public async Task<ServiceResponse<decimal>> GetBudget(int projectId)
+        {
+            var budget = 0m;
+
+            var result = await _artifactsService.GetAllByProjectId(projectId);
+
+            if(!result.Success) return new ServiceResponse<decimal>(new Error(ErrorCodes.ProjectNotExists, result.Error.Message));
+
+            foreach (var artifact in result.Value)
+            {
+                budget += artifact.GetPrice();
+            }
+
+            return new ServiceResponse<decimal>(budget);
+        }
+    }
+}
---
+++ FiveRockingFingers/FRF.Web/Controllers/BudgetController.cs
@@ -0,0 +1,30 @@
+﻿using FRF.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FRF.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    [Authorize]
+    public class BudgetController : ControllerBase
+    {
+        private readonly IBudgetService _budgetService;
+
+        public BudgetController(IBudgetService budgetService)
+        {
+            _budgetService = budgetService;
+        }
+
+        [HttpGet("{projectId}")]
+        public async Task<IActionResult> GetBudget(int projectId)
+        {
+            var response = await _budgetService.GetBudget(projectId);
+
+            if (!response.Success) return NotFound();
+
+            return Ok(response.Value);
+        }
+    }
+}
