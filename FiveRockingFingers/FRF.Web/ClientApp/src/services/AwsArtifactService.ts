9d196e2f3db1166d909a59a0675e1d2f09dd4268
Gonzalo Perelstein <gperelstein@makingsense.com>
2021-01-26 13:30:59 -0300

    [FIVE-299-AwsArtifactsProvider] GP: Se modificó las URLs de los endpoints del controller AwsArtifactsProviderController para que sigan las buenas prácticas de una api REST

--- FiveRockingFingers/FRF.Web/ClientApp/src/services/AwsArtifactService.ts
+++ FiveRockingFingers/FRF.Web/ClientApp/src/services/AwsArtifactService.ts
@@ -2,25 +2,25 @@ import axios from 'axios'
 import { BASE_URL } from '../Constants';
 import KeyValueStringPair from '../interfaces/KeyValueStringPair';
 
-const AWS_ARTIFACTS_PROVIDER_URL = `${BASE_URL}AwsArtifactsProvider/`;
+const AWS_ARTIFACTS_PROVIDER_URL = `${BASE_URL}awsArtifactsProvider`;
 
 export default class AwsArtifactsService {
 
     static GetNamesAsync = async () => {
         try {
-            return await axios.get(`${AWS_ARTIFACTS_PROVIDER_URL}GetNames`);
+            return await axios.get(`${AWS_ARTIFACTS_PROVIDER_URL}/names`);
         } catch (error) {
             return error.response ? error.response : error.message;
         }
     }
 
     static GetAttibutesAsync = async (serviceCode: string) => {
-        const response = await axios.get(`${AWS_ARTIFACTS_PROVIDER_URL}GetAttributes?serviceCode=${serviceCode}`);
+        const response = await axios.get(`${AWS_ARTIFACTS_PROVIDER_URL}/attributes?serviceCode=${serviceCode}`);
         return response;
     }
 
     static GetProductsAsync = async (serviceCode: string, artifactSettings: KeyValueStringPair[]) => {
-        const response = await axios.post(`${AWS_ARTIFACTS_PROVIDER_URL}GetProducts?serviceCode=${serviceCode}`, artifactSettings);
+        const response = await axios.post(`${AWS_ARTIFACTS_PROVIDER_URL}/products?serviceCode=${serviceCode}`, artifactSettings);
         return response;
     }
 }
