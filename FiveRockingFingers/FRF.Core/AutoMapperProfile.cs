2f34a37e6f164ee101bcd194653ce0b204efa105
Gonzalo Romero <gromero@makingsense.com>
2021-02-01 18:26:40 -0300

    [FIVE-297]GR:
    - Se creó clase AwsS3Artifact.
    - Se creó método GetPrice en AwsS3Artifact el cual implementa GetIntelligentPrice y GetStandardPrice.
    - Se creó método GetS3ProductsAsync y GetS3IntelligentTieringDetailList.

--- FiveRockingFingers/FRF.Core/AutoMapperProfile.cs
+++ FiveRockingFingers/FRF.Core/AutoMapperProfile.cs
@@ -1,5 +1,8 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Globalization;
+using AutoMapper;
 using FRF.Core.Models;
+using FRF.Core.Models.AwsArtifacts;
 
 namespace FRF.Core
 {
@@ -20,6 +23,37 @@ namespace FRF.Core
                 .ReverseMap();
             CreateMap<DataAccess.EntityModels.Artifact, Models.Artifact>()
                 .ReverseMap()
+                .ForMember(dest => dest.Id, act => act.Ignore())
+                .ForMember(dest => dest.ArtifactType, act => act.Ignore())
+                .ForMember(dest => dest.Project, act => act.Ignore());
+            CreateMap<DataAccess.EntityModels.Artifact, AwsS3Artifact>()
+                .ForMember(dest => dest.Id, act => act.Ignore())
+                .ForMember(dest => dest.ArtifactType, act => act.Ignore())
+                .ForMember(dest => dest.Project, act => act.Ignore())
+                .ForMember(dest => dest.WriteRequestsUsed, act =>
+                    act.MapFrom(ar => ar.Settings.Element("writeRequestsUsed").Value))
+                .ForMember(dest => dest.RetrieveRequestsUsed, act =>
+                    act.MapFrom(ar => ar.Settings.Element("retrieveRequestsUsed").Value))
+                .ForMember(dest => dest.StorageUsed, act =>
+                    act.MapFrom(ar => ar.Settings.Element("storageUsed").Value))
+                .ForMember(dest => dest.InfrequentAccessMultiplier, act =>
+                    act.MapFrom(ar => ar.Settings.Element("infrequentAccessMultiplier").Value))
+                .ForMember(dest => dest.StoragePrice, act =>
+                    act.MapFrom(ar =>
+                        Convert.ToDecimal(
+                            ar.Settings.Element("product1").Element("pricingDimensions").Element("range0")
+                                .Element("pricePerUnit").Value, CultureInfo.InvariantCulture)))
+                .ForMember(dest => dest.WriteRequestsPrice, act =>
+                    act.MapFrom(ar =>
+                        decimal.Parse(
+                            ar.Settings.Element("product2").Element("pricingDimensions").Element("pricePerUnit").Value,
+                            NumberStyles.Float)))
+                .ForMember(dest => dest.RetrieveRequestsPrice, act =>
+                    act.MapFrom(ar =>
+                        decimal.Parse(
+                            ar.Settings.Element("product3").Element("pricingDimensions").Element("pricePerUnit").Value,
+                            NumberStyles.Float)));
+            CreateMap<AwsS3Artifact, DataAccess.EntityModels.Artifact > ()
                 .ForMember(dest => dest.Id, act => act.Ignore())
                 .ForMember(dest => dest.ArtifactType, act => act.Ignore())
                 .ForMember(dest => dest.Project, act => act.Ignore());
