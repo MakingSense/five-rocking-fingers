a9a5ac6f604949fb65a9401fdb73310fe4588bad
martinmena <mmena@makingsense.com>
2021-02-03 15:44:29 -0300

    [FIVE-303] MM:
    - Creación de Script_Create_ProvidersAndUpdateArtifacts-ArtifactType con las modificaciones sobre la base de datos.
    - Se modificaron todos los EntityModels, Models y Dtos relacionados con los Artifacs y ArtifactTypes.

--- FiveRockingFingers/FRF.Web.Dtos/Artifacts/ArtifactUpsertDTO.cs
+++ FiveRockingFingers/FRF.Web.Dtos/Artifacts/ArtifactUpsertDTO.cs
@@ -5,7 +5,6 @@ namespace FRF.Web.Dtos.Artifacts
     public class ArtifactUpsertDTO
     {
         public string Name { get; set; }
-        public string Provider { get; set; }
         public XElement? Settings { get; set; }
         public int ProjectId { get; set; }
         public int ArtifactTypeId { get; set; }
