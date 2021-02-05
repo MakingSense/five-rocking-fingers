6cb06127bad081f0b9134a0105223d6bdeced190
Gonzalo Romero <gromero@makingsense.com>
2021-02-05 12:42:54 -0300

    [FIVE-297]GR: Mejoras en metodo GetPrice para adaptarse a formato de XML.
    - Se agrega property Product en PricingTerm para poder separar los distintos productos que componen un mismo precio del artefacto.

--- FiveRockingFingers/FRF.Core/AutoMapperProfile.cs
+++ FiveRockingFingers/FRF.Core/AutoMapperProfile.cs
@@ -24,11 +24,9 @@ namespace FRF.Core
             CreateMap<DataAccess.EntityModels.Artifact, Models.Artifact>()
                 .ReverseMap()
                 .ForMember(dest => dest.Id, act => act.Ignore())
-                .ForMember(dest => dest.ArtifactType, act => act.Ignore())
                 .ForMember(dest => dest.Project, act => act.Ignore());
             CreateMap<DataAccess.EntityModels.Artifact, AwsS3>()
                 .ForMember(dest => dest.Id, act => act.Ignore())
-                .ForMember(dest => dest.ArtifactType, act => act.Ignore())
                 .ForMember(dest => dest.Project, act => act.Ignore())
                 .ForMember(dest => dest.WriteRequestsUsed, act =>
                     act.MapFrom(ar => ar.Settings.Element("writeRequestsUsed").Value))
@@ -41,17 +39,17 @@ namespace FRF.Core
                 .ForMember(dest => dest.StoragePrice, act =>
                     act.MapFrom(ar =>
                         Convert.ToDecimal(
-                            ar.Settings.Element("product1").Element("pricingDimensions").Element("range0")
+                            ar.Settings.Element("product0").Element("pricingDimension").Element("range0")
                                 .Element("pricePerUnit").Value, CultureInfo.InvariantCulture)))
                 .ForMember(dest => dest.WriteRequestsPrice, act =>
                     act.MapFrom(ar =>
                         decimal.Parse(
-                            ar.Settings.Element("product2").Element("pricingDimensions").Element("pricePerUnit").Value,
+                            ar.Settings.Element("product1").Element("pricingDimension").Element("range0").Element("pricePerUnit").Value,
                             NumberStyles.Float)))
                 .ForMember(dest => dest.RetrieveRequestsPrice, act =>
                     act.MapFrom(ar =>
                         decimal.Parse(
-                            ar.Settings.Element("product3").Element("pricingDimensions").Element("pricePerUnit").Value,
+                            ar.Settings.Element("product2").Element("pricingDimension").Element("range0").Element("pricePerUnit").Value,
                             NumberStyles.Float)));
             CreateMap<AwsS3, DataAccess.EntityModels.Artifact > ()
                 .ForMember(dest => dest.Id, act => act.Ignore())
