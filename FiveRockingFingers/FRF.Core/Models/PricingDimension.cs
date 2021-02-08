901fe98b58e4600fb2285e95903c98c049a354ad
Gonzalo Romero <gromero@makingsense.com>
2021-02-08 12:50:06 -0300

    [FIVE-297]GR: Se elimina clase PricingDimensionPocos en desuso.

--- FiveRockingFingers/FRF.Core/Models/PricingDimension.cs
+++ FiveRockingFingers/FRF.Core/Models/PricingDimension.cs
@@ -4,11 +4,6 @@ using System.Text;
 
 namespace FRF.Core.Models
 {
-    public class PricingDimensionPocos
-    {
-        public Dictionary<string,PricingDimension> PricingDimensions { get; set; }
-    }
-
     public class PricingDimension
     {
         public string Unit { get; set; }
