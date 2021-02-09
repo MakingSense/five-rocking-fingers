a48a5f8e9c1831283e03960a5039788b9ccad23e
Gonzalo Romero <gromero@makingsense.com>
2021-02-09 17:32:30 -0300

    [FIVE-297]Merge con release

--- test/FRF.Core.Tests/Models/AwsArtifacts/AwsS3Test.cs
+++ test/FRF.Core.Tests/Models/AwsArtifacts/AwsS3Test.cs
@@ -187,21 +187,6 @@ namespace FRF.Core.Tests.Models
                 "\"range0\":{" +
                 "\"beginRange\":\"0\"," +
                 "\"currency\":\"USD\"," +
-                "\"description\":\"[MOCK] GB-Month of storage used in Intelligent-Tiering, Infrequent Access Tier\"," +
-                "\"endRange\":\"-1\"," +
-                "\"pricePerUnit\":\"" + infrequentPricePerUnit + "\"," +
-                "\"rateCode\":\"[MOCK] \"," +
-                "\"unit\":\"GB-Mo\"}}," +
-                "\"purchaseOption\":[]," +
-                "\"sku\":\"[MOCK] \"," +
-                "\"term\":\"OnDemand\"}," +
-                "\"product2\":{" +
-                "\"leaseContractLength\":[]," +
-                "\"offeringClass\":[]," +
-                "\"pricingDimension\":{" +
-                "\"range0\":{" +
-                "\"beginRange\":\"0\"," +
-                "\"currency\":\"USD\"," +
                 "\"description\":\"[MOCK]  per 1,000 PUT, COPY, POST, or LIST requests\"," +
                 "\"endRange\":\"-1\"," +
                 "\"pricePerUnit\":\"" + writeRequestsPrice + "\"," +
@@ -210,7 +195,7 @@ namespace FRF.Core.Tests.Models
                 "\"purchaseOption\":[]," +
                 "\"sku\":\"[MOCK] \"," +
                 "\"term\":\"OnDemand\"}," +
-                "\"product3\":{\"leaseContractLength\":[]," +
+                "\"product2\":{\"leaseContractLength\":[]," +
                 "\"offeringClass\":[]," +
                 "\"pricingDimension\":{" +
                 "\"range0\":{" +
@@ -221,6 +206,21 @@ namespace FRF.Core.Tests.Models
                 "\"pricePerUnit\":\"" + retrieveRequestsPrice + "\"," +
                 "\"rateCode\":\"[MOCK] \"," +
                 "\"unit\":\"Requests\"}}," +
+                "\"purchaseOption\":[]," +
+                "\"sku\":\"[MOCK] \"," +
+                "\"term\":\"OnDemand\"}," +
+                "\"product3\":{" +
+                "\"leaseContractLength\":[]," +
+                "\"offeringClass\":[]," +
+                "\"pricingDimension\":{" +
+                "\"range0\":{" +
+                "\"beginRange\":\"0\"," +
+                "\"currency\":\"USD\"," +
+                "\"description\":\"[MOCK] GB-Month of storage used in Intelligent-Tiering, Infrequent Access Tier\"," +
+                "\"endRange\":\"-1\"," +
+                "\"pricePerUnit\":\"" + infrequentPricePerUnit + "\"," +
+                "\"rateCode\":\"[MOCK] \"," +
+                "\"unit\":\"GB-Mo\"}}," +
                 "\"purchaseOption\":[]," +
                 "\"sku\":\"[MOCK] \"," +
                 "\"term\":\"OnDemand\"}," +
