9cc91c85df0a8b6dc87baf06834b6a0d44c9835f
Gonzalo Romero <gromero@makingsense.com>
2021-02-25 14:07:15 -0300

    [FIVE-327]GR: - Se creó compente NavMenuFilter el cual filtra los proyectos por categoría.
    - Se agregó hooks dentro NavMenu para gestionar los estados de los proyectos filtrados y los filtros seleccionados.

---
+++ FiveRockingFingers/FRF.Web/ClientApp/src/commons/NavMenuFilter.tsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+import { createMuiTheme, makeStyles, TextField, Theme, ThemeProvider } from "@material-ui/core";
+import { lime } from "@material-ui/core/colors";
+import { Autocomplete } from "@material-ui/lab";
+import Category from "../interfaces/Category";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  inputRoot: {
+    width: "90%",
+    marginTop: 0,
+    color: "#eceff1",
+    fontWeight: "bold",
+    background: "#546e7a",
+    "& .MuiOutlinedInput-notchedOutline": {
+      borderColor: lime[100],
+      border: `2px solid`
+    },
+    "&:hover .MuiOutlinedInput-notchedOutline": {
+      borderColor: lime[100],
+      border: `2px solid`
+    },
+    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      borderColor: lime[100],
+      border: `2px solid`
+    },
+  },
+}));
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiFormLabel: {
+      root: {
+        color: lime[100],
+        "&$focused": {
+          color: lime[100],
+        },
+      },
+    },
+  },
+});
+
+const NavMenuFilter = (props: {categories: Category[], setCategoriesFilter: Function ,cleaner: Function}) => {
+  const classes = useStyles();
+  const { categories, setCategoriesFilter, cleaner } = props;
+
+  const handleChange = (_event: React.ChangeEvent<{}>, value: Category[], reason: string) => {
+    if (reason === "clear") {
+      cleaner();
+      return;
+    }
+    if (value.length === 0) {
+      setCategoriesFilter([]);
+      return;
+    }
+    setCategoriesFilter(value);
+  };
+
+  return (
+    <Autocomplete
+      multiple
+      limitTags={1}
+      size="small"
+      classes={classes}
+      options={categories}
+      onChange={handleChange}
+      getOptionLabel={(option) => option.name}
+      renderInput={(params) => (
+        <ThemeProvider theme={theme}>
+          <TextField
+            {...params}
+            variant="outlined"
+            margin="normal"
+            label="Filtro por categorias:"
+          />
+        </ThemeProvider>
+      )}
+    />
+  );
+};
+
+export default NavMenuFilter;
