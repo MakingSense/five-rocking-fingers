7bb4a4ef11898ffdf2a99691f48ceaa689a5a99c
Gonzalo Romero <gromero@makingsense.com>
2021-03-12 15:23:00 -0300

    [FIVE-351]GR:
    - Se elimina using deprecado

--- test/FRF.Web.Tests/Authorization/ArtifactsListOwnershipHandlerTest.cs
+++ test/FRF.Web.Tests/Authorization/ArtifactsListOwnershipHandlerTest.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using FRF.Core.Tests;
 using FRF.DataAccess;
 using FRF.DataAccess.EntityModels;
 using FRF.Web.Authorization;
