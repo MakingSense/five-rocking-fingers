c206286f46b7f52cc616e619fb5f8827eca5f8e7
Gonzalo Perelstein <gperelstein@makingsense.com>
2021-03-18 09:56:13 -0300

    [FIVE-331-Frontend] GP:
    -Se instaló la librería react-jsonschema-form
    -Se agregó el método GetRequiredFieldsAsync en el servicio del frontend AwsArtifactService
    -Se modificó el componente SettingsAwsForm para renderizar el JSON que viene en el campo required fields de un artifact type

--- FiveRockingFingers/FRF.Web/ClientApp/src/components/NewArtifactDialogComponents/SettingsAwsForm.tsx
+++ FiveRockingFingers/FRF.Web/ClientApp/src/components/NewArtifactDialogComponents/SettingsAwsForm.tsx
@@ -7,6 +7,8 @@ import ProviderArtifactSetting from '../../interfaces/ProviderArtifactSetting';
 import Setting from '../../interfaces/Setting';
 import KeyValueStringPair from '../../interfaces/KeyValueStringPair';
 import AwsArtifactService from '../../services/AwsArtifactService';
+import { withTheme } from "@rjsf/core";
+import { Theme as MaterialUITheme } from "@rjsf/material-ui";
 
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -36,26 +38,28 @@ const useStyles = makeStyles((theme: Theme) =>
 const SettingsAwsForm = (props: { showNewArtifactDialog: boolean, closeNewArtifactDialog: Function, name: string | null, updateList: Function, handleNextStep: Function, handlePreviousStep: Function, setAwsSettingsList: Function, setSettingsList: Function, setSnackbarSettings: Function, setOpenSnackbar: Function }) => {
 
     const classes = useStyles();
+    const Form = withTheme(MaterialUITheme);
     const { handleSubmit, errors, setError, clearErrors, control } = useForm();
     const { showNewArtifactDialog, closeNewArtifactDialog, name } = props;
 
     //Hook for save the user's settings input
-    const [awsSettingsValuesList, setAwsSettingsValuesList] = React.useState<ProviderArtifactSetting[]>([]);
     const [loading, setLoading] = React.useState<Boolean>(true);
     const [awsSettingsList, setAwsSettingsList] = React.useState<KeyValueStringPair[]>([]);
 
+    const [schema, setSchema] = React.useState<object>({});
+
     React.useEffect(() => {
-        getArtifactSettings();
+        getRequireFields();
     }, [name]);
 
-    const getArtifactSettings = async () => {
+    const getRequireFields = async () => {
         if (name) {
             let statusOk = 200;
             try {
                 setLoading(true);
-                const response = await AwsArtifactService.GetAttibutesAsync(name);
+                const response = await AwsArtifactService.GetRequiredFieldsAsync(name);
                 if (response.status === statusOk) {
-                    setAwsSettingsValuesList(response.data);
+                    setSchema(response.data);
                     setLoading(false);
                 }
             }
@@ -68,12 +72,13 @@ const SettingsAwsForm = (props: { showNewArtifactDialog: boolean, closeNewArtifa
     }
 
     //Create the artifact after submit
-    const handleConfirm = async () => {
-        let awsSettingsListFiltered = awsSettingsList.filter(awsSetting => awsSetting !== undefined && awsSetting !== null && awsSetting.value !== "");
+    const handleConfirm = async (data: any) => {
+        /*let awsSettingsListFiltered = awsSettingsList.filter(awsSetting => awsSetting !== undefined && awsSetting !== null && awsSetting.value !== "");
         console.log(awsSettingsListFiltered);
         props.setSettingsList(awsSettingsListToSettingsList(awsSettingsListFiltered));
         props.setAwsSettingsList(awsSettingsListFiltered);
-        props.handleNextStep();
+        props.handleNextStep();*/
+        console.log(data.formData);
     }
 
     //Handle changes in the inputs fields
@@ -110,64 +115,24 @@ const SettingsAwsForm = (props: { showNewArtifactDialog: boolean, closeNewArtifa
 
     return (
         <Dialog open={showNewArtifactDialog}>
-            <DialogTitle id="alert-dialog-title">Formulario de artefactos AWS</DialogTitle>
             <DialogContent>
-                <Typography gutterBottom>
-                    A continuación ingrese las propiedades de su nuevo artefacto AWS
-                </Typography>
                 {loading ?
                     <div className={classes.circularProgress}>
                         <CircularProgress color="inherit" size={30} />
                     </div> :
-                    <form className={classes.container}>
-                        {awsSettingsValuesList.map((awsSetting: ProviderArtifactSetting, index: number) => {
-                            return (
-                                <FormControl key={index} className={classes.formControl}>
-                                    <InputLabel htmlFor={`settings[${index}].name`}>{awsSetting.name.value}</InputLabel>
-                                    <Controller
-                                        render={({ onChange, value }) => (
-                                            <Select
-                                                labelId={`settings[${index}].name`}
-                                                inputProps={{
-                                                    name: `settings[${index}].name`,
-                                                    id: `settings[${index}].name`
-                                                }}
-                                                onChange={event => handleInputChange(awsSetting.name.key, event.target.value as string, index)}
-                                                name={`settings[${index}].name`}
-                                                defaultValue={''}
-                                                value={awsSettingsList[index] === undefined ? '' : awsSettingsList[index].value}
-                                                error={errors.settings && errors.settings[index] && typeof errors.settings[index]?.name !== 'undefined'}
-                                            >
-                                                <MenuItem value={''}>
-                                                    None
-                                                </MenuItem>
-                                                {awsSetting.values.map(p => <MenuItem key={p} value={p}>{p}</MenuItem>)}
-                                            </Select>
-                                        )}
-                                        name={`settings[${index}].name`}
-                                        rules={{ validate: { isValid: () => isOneValueCompleted() } }}
-                                        control={control}
-                                        defaultValue={''}
-                                        value={awsSettingsList[index] === undefined ? '' : awsSettingsList[index].value}
-                                        error={errors.settings && errors.settings[index] && typeof errors.settings[index]?.name !== 'undefined'}
-                                    />
-                                </FormControl>
-                            );
-                        })}
-                        {errors.settings ?
-                            <Typography gutterBottom className={classes.error}>
-                                Al menos un campo debe ser completado
-                            </Typography> : null
-                        }
-                    </form>
+                    <Form
+                        id="my-form"
+                        schema={schema}
+                        onSubmit={handleConfirm}
+                    >
+                        <div>
+                            <Button size="small" color="primary" onClick={event => goPrevStep()}>Atrás</Button>
+                            <Button size="small" color="primary" type="submit">Siguiente</Button>
+                            <Button size="small" color="secondary" onClick={handleCancel}>Cancelar</Button>
+                        </div>
+                    </Form>
                 }
-                
             </DialogContent>
-            <DialogActions>
-                <Button size="small" color="primary" onClick={event => goPrevStep()}>Atrás</Button>
-                <Button size="small" color="primary" type="submit" onClick={handleSubmit(handleConfirm)}>Siguiente</Button>
-                <Button size="small" color="secondary" onClick={handleCancel}>Cancelar</Button>
-            </DialogActions>
         </Dialog>
     );
 }
