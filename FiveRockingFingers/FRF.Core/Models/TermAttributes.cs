19e4fb6cda542627737c5ec34d3ee52b18542f0e
gperelstein <71278846+gperelstein@users.noreply.github.com>
2021-03-19 13:43:55 -0300

    [Five-360] Modificar la implemetación del servicio Amazon EC2 (#122)
    Cambios realizados
    
    -Se modificó el xml de las settings correspondiente a los tipos de artefactos AmazonEC2 para que las settings relacionables están por fuera de los nodos "product"
    -Se modificó el modelo AwsEc2 para incorporar 3 nodos correspondiente al número de Gb correspondientes al data transfer y que estos datos queden fuera del nodo product5
    -Se modificó el modelo AwsEc2 para incorporar 3 nodos correspondientes al precio de los servicios de data transfer llamados product5-1, product5-2 y product5-3
    -Se modificó el schema AwsEc2.xsd para reflejar las modificaciones anteriormente mencionadas
    -Se creó el modelo TermAttributes para guardar si el producto es adquirido OnDemand o Reserved (TermType), tiempo de en el que se contrata el servicio (LeaseContractLength), si la instancia es convertible o estándar (OfferingClass) y si el pago se hace por adelantado o no (PurchaseOption)
    -Se agregaron constantes en la clase AwsEc2Descriptions relacionadas con los atributos de la clase TermAttributes y con la posibilidad que tiene el usuario de agregar 3 productos de transferencia de datos
    -Se cambió el método GetEc2ProductsAsync para agregar la posibilidad de que el usuario seleccione 3 productos de transferencia de datos
    -Se modificó el método GetEc2ProductsAsync para que solo llegue una opción de cada producto que forma el precio de servidor de Amazon EC2 (excepto por la transferencia de datos que se contempla hasta 3)

---
+++ FiveRockingFingers/FRF.Core/Models/TermAttributes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FRF.Core.Models
+{
+    public class TermAttributes
+    {
+        public string TermType { get; set; }
+        public string LeaseContractLength { get; set; }
+        public string OfferingClass { get; set; }
+        public string PurchaseOption { get; set; }
+    }
+}
