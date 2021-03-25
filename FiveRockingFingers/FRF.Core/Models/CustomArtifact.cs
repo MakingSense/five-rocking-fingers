2db8561d90de91e5cc09ab6c6cfebb1c86ebec84
martinmena <mmena@makingsense.com>
2021-03-25 12:38:46 -0300

    [FIVE-359] MM:
    - Se crean validadores de SettingTypes dentro del archivo con las constantes de SettingTypes.
    - En el constructor de CustomArtifact se agrega la lógica para crear el diccionario RelationalFields con los datos del XML.
    - En CustomArtifactTests se añade un nuevo test verificando que la creación de dicho diccionario se ejecute de forma correcta.
    - En ArtifactsService se crean las funciones SetSettingTypesXML y ValidTypeSettings, y se utilizan ambas para validar las settings de artefactos custom en la creación y modificación.
    - Se crean nuevos tests en ArtifactsServiceTests para validar dicha funcionalidad nueva.

--- FiveRockingFingers/FRF.Core/Models/CustomArtifact.cs
+++ FiveRockingFingers/FRF.Core/Models/CustomArtifact.cs
@@ -1,7 +1,23 @@
-﻿namespace FRF.Core.Models
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace FRF.Core.Models
 {
     public class CustomArtifact : Artifact
     {
+        public CustomArtifact(XElement settings)
+        {
+            Settings = settings;
+
+            RelationalFields = new Dictionary<string, string>();
+            foreach (XElement xe in Settings.Elements())
+            {
+                if (xe.Attribute("type") != null)
+                {
+                    RelationalFields.Add(xe.Name.ToString(), xe.Attribute("type").Value.ToString());
+                }
+            }
+        }
         public override decimal GetPrice()
         {
             if (Settings.Element("price") != null)
--- test/FRF.Core.Tests/Models/CustomArtifactTests.cs
+++ test/FRF.Core.Tests/Models/CustomArtifactTests.cs
@@ -1,4 +1,5 @@
 ﻿using FRF.Core.Models;
+using System.Linq;
 using System.Xml.Linq;
 using Xunit;
 
@@ -6,19 +7,13 @@ namespace FRF.Core.Tests.Models
 {
     public class CustomArtifactTests
     {
-        private readonly CustomArtifact _classUnderTest;
-
-        public CustomArtifactTests()
-        {
-            _classUnderTest = new CustomArtifact();
-        }
-
         [Fact]
         public void GetPrice_priceNodeExists_ReturnsPrice()
         {
             // Arrange
             var price = 70;
-            _classUnderTest.Settings = XElement.Parse($"<settings><price>{price}</price></settings>");
+            var settings = XElement.Parse($"<settings><price>{price}</price></settings>");
+            var _classUnderTest = new CustomArtifact(settings);
 
             // Act
             var result = _classUnderTest.GetPrice();
@@ -31,13 +26,36 @@ namespace FRF.Core.Tests.Models
         public void GetPrice_priceNodeNotExists_Returns0()
         {
             // Arrange
-            _classUnderTest.Settings = XElement.Parse("<settings></settings>");
+            var settings = XElement.Parse("<settings></settings>");
+            var _classUnderTest = new CustomArtifact(settings);
 
             // Act
             var result = _classUnderTest.GetPrice();
 
             // Assert
             Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void RelationalFieldsGeneratedCorrectly()
+        {
+            // Arrange
+            XElement settings = new XElement("settings",
+                    new XElement("settingName1", 100, new XAttribute("type", SettingTypes.Decimal)),
+                    new XElement("settingName2", 200, new XAttribute("type", SettingTypes.NaturalNumber)),
+                    new XElement("settingName3", 300, new XAttribute("type", SettingTypes.Decimal)));
+
+            // Act
+            var _classUnderTest = new CustomArtifact(settings);
+
+            // Assert
+            var settingsList = settings.Elements().ToList();
+
+            Assert.Equal(settingsList.Count, _classUnderTest.RelationalFields.Count);
+            foreach(var setting in settingsList)
+            {
+                Assert.Equal(setting.Attribute("type").Value.ToString(), _classUnderTest.RelationalFields[setting.Name.ToString()]);
+            }            
         }
     }
 }
