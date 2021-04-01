b771c734429a432f9829a040cd3bf506974386dd
martinmena <71275374+martinmena@users.noreply.github.com>
2021-04-01 13:07:37 -0300

    [FIVE-343] (FrontEnd) Crear control de tipo en el valor de las settings y relaciones (#143)
    - Se crea el componente HelperArtifactRelation con la funcionalidad compartida entre NewArtifactsRelation y EditArtifactRelation, extrayendola de dichos componentes.
    - En ambos componentes, previo a crear o modificar relaciones, se añade la verificación de que los tipos de setting sean iguales.
    - En Constants.tsx, se crean constantes específicas con el nombre de cada proveedor, y el arreglo PROVIDERS se llena con ellas.

--- FiveRockingFingers/FRF.Web/ClientApp/src/Constants.tsx
+++ FiveRockingFingers/FRF.Web/ClientApp/src/Constants.tsx
@@ -1,8 +1,11 @@
 ﻿// Constants definitions for access on ClientApp
 
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const PROVIDERS = ['AWS', 'Custom'];
 export const SETTINGTYPES = ['decimal', 'naturalNumber'];
+
+export const CUSTOM_PROVIDER = 'Custom';
+export const AWS_PROVIDER = 'AWS';
+export const PROVIDERS = [AWS_PROVIDER, CUSTOM_PROVIDER];
 
 //Amazon EC2 constants
 export const AMAZON_EC2 = 'AmazonEC2';
