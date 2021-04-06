fe0300f04553f7d23f49780f0696141abb625b78
Gonzalo Romero <71275081+gonzalorom@users.noreply.github.com>
2021-04-06 09:32:33 -0300

    [FIVE-393] Agregar welcome page a componente home
    - Se creó pantalla de bienvenida en componente Home.
    - Se agregó símbolo de exclamación en validación de contraseña

--- FiveRockingFingers/FRF.Web/ClientApp/src/components/auth/Login.tsx
+++ FiveRockingFingers/FRF.Web/ClientApp/src/components/auth/Login.tsx
@@ -28,7 +28,7 @@ const UserLoginSchema = yup.object().shape({
     password: yup.string()
         .trim()
         .required('Requerido.')
-        .matches(/(?=.*[@#$%^&+=.\-_*])/, 'Debe incluir al menos un simbolo distinto de + o =')
+        .matches(/(?=.*[@#$%^&+=.\-_!*])/, 'Debe incluir al menos un simbolo distinto de + o =')
         .test("hasPlusOrEqual","No puede incluir los simbolos + o =",(password)=> !/(\+|=)/.test(password!))
         .test("hasAnyNumber","Debe incluir al menos un numero.",(password)=> /(?=.*\d)/.test(password!))
         .test("hasAnyUppercase","Debe incluir al menos un caracter en mayuscula.",(password)=> /(?=.*[A-Z])/.test(password!))
