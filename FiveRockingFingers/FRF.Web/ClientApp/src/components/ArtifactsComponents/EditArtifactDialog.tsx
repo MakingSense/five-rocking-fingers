ac298202791421ed82cfd64e8ba794303a3818be
Gonzalo Perelstein <gperelstein@makingsense.com>
2021-04-13 13:19:47 -0300

    [FIVE-397-EditArtifactsComponents] GP:
    -Se modificaron los componentes EditArtifactConfirmation y EditArtifactDialog para hacer uso del hook useArtifact
    -Se modificó el componente EditArtifact para hacer uso del hook useArtifact y del componente SettingsEntries

--- FiveRockingFingers/FRF.Web/ClientApp/src/components/ArtifactsComponents/EditArtifactDialog.tsx
+++ FiveRockingFingers/FRF.Web/ClientApp/src/components/ArtifactsComponents/EditArtifactDialog.tsx
@@ -5,6 +5,7 @@ import ArtifactService from '../../services/ArtifactService';
 import ArtifactRelation from '../../interfaces/ArtifactRelation';
 import EditArtifact from './EditArtifact';
 import EditArtifactConfirmation from './EditArtifactConfirmation';
+import { useArtifact } from '../../commons/useArtifact';
 
 
 const EditArtifactDialog = (props: {
@@ -18,14 +19,16 @@ const EditArtifactDialog = (props: {
     settingTypes: { [key: string]: string }, 
     setSettingTypes: Function  }) => {
 
-    const { showEditArtifactDialog, closeEditArtifactDialog, settingTypes, setSettingTypes } = props;
+    const { showEditArtifactDialog, closeEditArtifactDialog, settingTypes } = props;
 
     const [isArtifactEdited, setIsArtifactEdited] = React.useState<boolean>(false);
     const [artifactEdited, setArtifactEdited] = React.useState<Artifact>(props.artifactToEdit);
     const [namesOfSettingsChanged, setNamesOfSettingsChanged] = React.useState<string[]>([]);
     const [artifactsRelations, setArtifactsRelations] = React.useState<ArtifactRelation[]>([]);
+    const [firstRender, setFirstRender] = React.useState<boolean>(true);
 
-    const [originalSettingTypes, setOriginalSettingTypes] = React.useState<{ [key: string]: string }>();
+    const [originalSettingTypes, setOriginalSettingTypes] = React.useState<{ [key: string]: string }>({});
+    const { resetStateOnClose, setSettingTypes } = useArtifact();
 
     const getRelations = async () => {
         try {
@@ -44,8 +47,17 @@ const EditArtifactDialog = (props: {
         }
     }
 
+    const closeDialog = () => {
+        resetStateOnClose();
+        closeEditArtifactDialog();
+    } 
+
     React.useEffect(() => {
-        getRelations();
+        if (firstRender) {
+            setSettingTypes(props.settingTypes);
+            getRelations();
+            setFirstRender(false);
+        }        
     }, [artifactEdited]);
 
     React.useEffect(() => {
@@ -57,22 +69,19 @@ const EditArtifactDialog = (props: {
             {!isArtifactEdited ?
                 <EditArtifact
                     artifactToEdit={props.artifactToEdit}
-                    closeEditArtifactDialog={closeEditArtifactDialog}
+                    closeEditArtifactDialog={closeDialog}
                     setIsArtifactEdited={setIsArtifactEdited}
                     setArtifactEdited={setArtifactEdited}
                     setNamesOfSettingsChanged={setNamesOfSettingsChanged}
                     artifactsRelations={artifactsRelations}
-                    settingTypes={settingTypes}
-                    setSettingTypes={setSettingTypes}
                 /> :
                 <EditArtifactConfirmation
                     artifactToEdit={artifactEdited}
                     namesOfSettingsChanged={namesOfSettingsChanged}
-                    closeEditArtifactDialog={closeEditArtifactDialog}
+                    closeEditArtifactDialog={closeDialog}
                     setOpenSnackbar={props.setOpenSnackbar}
                     setSnackbarSettings={props.setSnackbarSettings}
                     updateArtifacts={props.updateArtifacts}
-                    settingTypes={settingTypes}
                     originalSettingTypes={originalSettingTypes}
                 />
             }
