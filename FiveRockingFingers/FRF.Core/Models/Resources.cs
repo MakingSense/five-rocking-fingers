6a2df940dbc01979ffb0cd129d5bacd46ae83726
Gonzalo Romero <gromero@makingsense.com>
2021-04-13 09:42:13 -0300

    [FIVE-401]GR:
    - Se corrigió typo en RoleName.

--- FiveRockingFingers/FRF.Core/Models/Resources.cs
+++ FiveRockingFingers/FRF.Core/Models/Resources.cs
@@ -3,7 +3,7 @@
     public class Resources
     {
         public int Id { get; set; }
-        public string RollName { get; set; }
+        public string RoleName { get; set; }
         public string? Description { get; set; }
         public decimal SalaryPerMonth { get; set; }
         public int WorkloadCapacity { get; set; }
