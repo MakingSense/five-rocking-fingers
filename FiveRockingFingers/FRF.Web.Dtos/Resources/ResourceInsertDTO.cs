f58e42a260f5088b3c24574d7d9e81b387b4c664
Gonzalo Romero <gromero@makingsense.com>
2021-04-16 18:23:28 -0300

    [FIVE-403]GR:
    - Se creó ResourceController.
    - Se creó ResourceDTO, ResourceInsertDTO y ResourceUpdateDTO.
    - Se agregaron nuevos DTOs al AutoMapper.

---
+++ FiveRockingFingers/FRF.Web.Dtos/Resources/ResourceInsertDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FRF.Web.Dtos.Resources
+{
+    public class ResourceInsertDTO
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        [Range(1,100000000000, ErrorMessage = "Salary have to be greater than {1}")]
+        public decimal SalaryPerMonth { get; set; }
+        [Required]
+        public int WorkloadCapacity { get; set; }
+    }
+}
