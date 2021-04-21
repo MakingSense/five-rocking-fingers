f12b02f1cad70cd64804eb082c95e812b8680bba
Gonzalo Perelstein <gperelstein@makingsense.com>
2021-04-21 09:06:30 -0300

    [FIVE-404-Controller] GP: Se agregaron las data annotations al DTO ProjectResourceUpsertDTO

--- FiveRockingFingers/FRF.Web.Dtos/ProjectResources/ProjectResourceUpsertDTO.cs
+++ FiveRockingFingers/FRF.Web.Dtos/ProjectResources/ProjectResourceUpsertDTO.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace FRF.Web.Dtos.ProjectResources
 {
     public class ProjectResourceUpsertDTO
     {
+        [Required]
+        [Range(1, int.MaxValue)]
         public int ProjectId { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
         public int ResourceId { get; set; }
+        [Required]
         public int DedicatedHours { get; set; }
         public DateTime? BeginDate { get; set; }
         public DateTime? EndDate { get; set; }
