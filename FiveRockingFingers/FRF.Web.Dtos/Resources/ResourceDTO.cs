413605b86270982e7e942a55d3466103e1c7f1f3
Gonzalo Romero <71275081+gonzalorom@users.noreply.github.com>
2021-04-23 17:27:54 -0300

    [FIVE-403_Controller] Crear Servicio y Controller de Resources (#158)
    - Se creó ResourceController.
    - Se creó ResourceDTO y ResourceUpsertDTO.
    - Se agregaron nuevos DTOs al AutoMapper.
    - Se agregó unit test de ResourcesController.
    - Se modificó naming ResourceController a ResourcesController

---
+++ FiveRockingFingers/FRF.Web.Dtos/Resources/ResourceDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace FRF.Web.Dtos.Resources
+{
+    public class ResourceDTO
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+        public string? Description { get; set; }
+        public decimal SalaryPerMonth { get; set; }
+        public int WorkloadCapacity { get; set; }
+    }
+}
